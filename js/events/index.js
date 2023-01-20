330975b088ffc2558e8afa06e1a91c6e73322e8f
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-01-20 15:53:58 -0300

    File name fixed

--- js/events/index.js
+++ js/events/index.js
@@ -1,5 +1,5 @@
 import {} from "./onSubmitLogin.js";
-import {} from "./onclickMenu.js";
+import {} from "./onClickMenu.js";
 import {} from "./onSubmitMessage.js";
 import { onClickAsideOptions } from "./onClickAsideOptions.js";
 
